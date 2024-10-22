442a3d93516814606eb83747a2f32e3533b9ad33
samruddishetty <65382899+samruddishetty@users.noreply.github.com>
2024-10-22 20:11:40 +0100

    setup

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ def get_requirements(file_path:str)->List[str]:
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
+    return requirements 
+
 
 
 #This is like metadata version with all information about the project
